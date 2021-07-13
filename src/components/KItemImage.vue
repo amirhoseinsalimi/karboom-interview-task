cf5ab0a339e892d840d2d3fa2b849798cff33147
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 00:12:09 +0430

    Implement "KItemContent" component

---
+++ src/components/KItemImage.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    class="k-item-image"
+  />
+</template>
+
+<script>
+export default {
+  name: 'KItemImage',
+
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+$size: 60px;
+
+.k-item-image {
+  border: {
+    radius: 10px;
+  };
+  background: {
+    color: #bbb;
+  };
+  width: $size;
+  height: $size;
+}
+</style>
