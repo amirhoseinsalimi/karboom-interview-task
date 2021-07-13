2ed3676127ece9c2a689b01396942c16dcefccb8
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 00:18:29 +0430

    Implement "KItemContent" component

---
+++ src/components/KItemContent.vue
@@ -0,0 +1,41 @@
+<template>
+  <h4 class="k-item-content__title">
+    {{ title }}
+  </h4>
+  <p class="k-item-content__title">
+    {{ description }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'KItemContent',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+* {
+  display: inline-block;
+}
+
+.k-item-content__title {
+  font: {
+    weight: bold;
+  }
+}
+
+.k-item-content__description {
+  color: #aaa;
+}
+</style>
