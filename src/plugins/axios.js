a503df5cf3076b6b9a77f418e7304068a135da92
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 20:34:04 +0430

    Add default headers to axios config

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+axios.defaults.headers.common = {
+  'Content-Type': 'application-json',
+  Accept: 'application-json',
+};
+
 export default axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
+  baseURL: process.env.VUE_APP_BASE_URL,
 });
