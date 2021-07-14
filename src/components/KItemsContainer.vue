263f4e2415c4357d8f5c47215552ea02144ab144
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 17:38:47 +0430

    Add functionality to fetch all items and delete items

--- src/components/KItemsContainer.vue
+++ src/components/KItemsContainer.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="k-items-container">
-    <k-item v-for="item of 2" :key="item">
-      {{ item }}
-    </k-item>
+    <k-item
+      v-for="{
+        id,
+        title: { text: title },
+        description,
+        img: src,
+        color
+      } of items"
+      :key="`${id}-${title}`"
+      :id="+id"
+      :title="title"
+      :description="description"
+      :src="src"
+      alt="lorem ipsum"
+      :color="color"
+      @delete="promptDeleteItem(id)"
+    />
   </div>
+
+  <k-modal
+    v-if="modal.displayed"
+    @accept="deleteItem(itemToBeDeleted)"
+    @refuse="modal.displayed = false"
+    header="Are you sure?"
+    :body='`The item "${items[itemToBeDeleted].title.text}" will be deleted. This action cannot be undone.`'
+    accept-text="Delete"
+    refuse-text="Cancel"
+  />
 </template>
 
 <script>
-import KItem from './KItem';
+import KModal from '@/components/KModal.vue';
+import KItem from '@/components/KItem.vue';
+
+import RestService from '../services/RestService';
 
 export default {
   name: 'KItemsContainer',
 
-  components: { KItem }
+  components: { KItem, KModal },
+
+  data() {
+    return {
+      items: [],
+      modal: {
+        displayed: false,
+        header: '',
+        body: '',
+      },
+      itemToBeDeleted: 0,
+    }
+  },
+
+  methods: {
+    promptDeleteItem(id) {
+      this.itemToBeDeleted = id;
+      this.modal.displayed = true;
+    },
+
+    deleteItem(id) {
+      RestService.delete(id);
+    },
+  },
+
+  async beforeCreate() {
+    try {
+      this.items = await RestService.index('/data');
+    } catch (e) {
+      // TODO: Handle error
+      console.log(e)
+    }
+  },
 }
 </script>
 
