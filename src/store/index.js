dd438aed4150b78e0796334f37c07451619a43f8
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 18:46:03 +0430

    Add fields for `searchText` and `biggestItemId` and their corresponding mutations

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,17 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    searchText: '',
+    biggestItemId: '',
   },
   mutations: {
+    storeSearchText(state, text) {
+      state.searchText = text;
+    },
+
+    changeBiggestItemId(state, id) {
+      state.biggestItemId = +id;
+    }
   },
   actions: {
   },
