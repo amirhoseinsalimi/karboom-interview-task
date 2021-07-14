037731537791163ec22af278f1ef4d81b5c0cf7e
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 13:29:38 +0430

    KSearchBar

---
+++ src/components/KSearchBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="k-search-bar">
+    <k-text-input placeholder="Search something...">
+      <template v-slot:icon>
+        <img src="@/assets/svgs/search.svg" alt="Search icon" />
+      </template>
+    </k-text-input>
+
+    <k-button
+        :circular="true"
+        backgroundColor="#F0F0F0"
+        size="sm"
+        :disabled="false"
+    >
+      <img src="@/assets/svgs/plus.svg" alt="Search icon" />
+    </k-button>
+  </div>
+</template>
+
+<script>
+import KTextInput from '@/components/KTextInput.vue';
+import KButton from "@/components/KButton.vue";
+
+export default {
+  name: 'KSearchBar',
+
+  components: {
+    KTextInput,
+    KButton,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.k-search-bar {
+  background: #DCE2E8;
+  border: {
+    radius: 8px;
+  }
+  padding: 2px 10px;
+  width: 400px;
+  display: inline-flex;
+  flex-flow: row-reverse wrap;
+  justify-content: space-around;
+  align-items: center;
+
+  .k-text-input {
+    flex-grow: 1;
+  }
+}
+</style>
