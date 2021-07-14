01835390ac80035147344a550c8a7426f7f170f2
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-07-14 05:35:34 +0430

    Implement the initial "KItemsContainer" component

---
+++ src/components/KItemsContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="k-items-container">
+    <k-item v-for="item of 2" :key="item">
+      {{ item }}
+    </k-item>
+  </div>
+</template>
+
+<script>
+import KItem from './KItem';
+
+export default {
+  name: 'KItemsContainer',
+
+  components: { KItem }
+}
+</script>
+
+<style scoped lang="scss">
+.k-items-container {
+  display: inline-block;
+  width: 600px;
+  max-width: 90%;
+}
+</style>
